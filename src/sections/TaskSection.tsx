30885ff94e8cbcc10cfe58297a8eabe7a6868d34
Keiler Guardo <keilerguardo@gmail.com>
2024-12-15 15:58:43 -0500

    feat: improving styling of the project

--- src/sections/TaskSection.tsx
+++ src/sections/TaskSection.tsx
@@ -12,13 +12,18 @@ export const TaskSection = (): ReactNode => {
   };
 
   return (
-    <section className="w-full">
+    <section className="w-full mt-5">
       <div className="relative">
         <InputTask onNewTask={onAddTask} />
 
-        {tasks.map((task: string) => (
-          <Task key={task} name={task} />
-        ))}
+        <div className="mt-10">
+          {tasks.map((task: string) => (
+            <div className="mt-2">
+              <Task key={task} name={task} />
+            </div>
+          ))}
+        </div>
+
         {/* <!-- <Button className="absolute right-0 top-0"> */}
         {/* <CircleArrowDown size={32} /> */}
         {/* </Button> --> */}
